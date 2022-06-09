20689558fe899797dd2616e61b2c2efc80c6197b
EricM <101633226+EricMauler@users.noreply.github.com>
2022-06-09 09:42:19 +0200

    first commit

---
+++ SpringShopJpa-1/src/test/java/fr/fms/SpringShopJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
